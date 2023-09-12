fbfd3e333d13606d377d77337111fd7f64c35bfc
채종원 <cowhddnjsdl0@naver.com>
2023-09-12 18:25:51 +0900

    fix: class kind

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -26,13 +26,16 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.builder(
+            return ListView.separated(
               scrollDirection: Axis.horizontal,
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 final webtoon = snapshot.data![index];
                 return Text(webtoon.title);
               },
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 80,
+              ),
             );
           } else {
             return const Center(
